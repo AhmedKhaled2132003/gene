5940952adddc2442f555db4c9c6a2d4a1de1842f
AhmedKhaled2132003 <ak01030130741@gmail.com>
2025-05-15 10:33:20 +0300

    Update KHLD-generatorlinks.user.js

--- KHLD-generatorlinks.user.js
+++ KHLD-generatorlinks.user.js
@@ -1,24 +1,3 @@
-// ==UserScript==
-// @name         Family Farm Gift Link Generator
-// @namespace    http://tampermonkey.net/
-// @version      3.0
-// @description  Generate gift links for Family Farm game with URL shortening and scheduling
-// @author       Dr. Ahmed Khaled
-// @match        *://*/*
-// @updateURL    https://raw.githubusercontent.com/AhmedKhaled2132003/gene/edit/main/KHLD-generatorlinks.user.js
-// @downloadURL  https://raw.githubusercontent.com/AhmedKhaled2132003/gene/edit/main/KHLD-generatorlinks.user.js
-// @grant        GM_xmlhttpRequest
-// @grant        GM_download
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_notification
-// @grant        GM_setClipboard
-// @connect      tinyurl.com
-// @connect      farm-us.centurygames.com
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @resource     NOTIFICATION_SOUND https://files.catbox.moe/hb2et0.mp3
-// ==/UserScript==
-
 (async function () {
     'use strict';
 
